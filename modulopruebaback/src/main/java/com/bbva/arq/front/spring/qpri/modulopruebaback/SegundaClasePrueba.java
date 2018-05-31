fdbfcf567557f2f9da0cd1636e2308af259f1c81
Luis Gomez <ing.luis.gomez.catalan@gmail.com>
2018-05-31 12:52:41 -0500

    first commit

---
+++ modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/SegundaClasePrueba.java
@@ -0,0 +1,13 @@
+package com.bbva.arq.front.spring.qpri.modulopruebaback;
+
+public interface SegundaClasePrueba {
+  
+  public methodo(){
+  
+  }
+  
+  public methodo3(){
+  
+  }
+
+}
---
+++ modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
@@ -0,0 +1,15 @@
+package com.bbva.arq.front.spring.qpri.modulopruebaback.impl;
+
+import com.bbva.arq.front.spring.qpri.modulopruebaback.ClasePruebaBack;
+
+public ClasePruebaBackImpl implements ClasePruebaBack {
+  
+  private String valor(){
+  
+  }
+  
+  private void valor(){
+    return "valor que no debe de regresar";
+  }
+
+}
