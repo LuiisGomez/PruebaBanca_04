f472bf4f4371440fee0058f0c9788bf1829848d9
LuiisGomez <39528658+LuiisGomez@users.noreply.github.com>
2018-05-31 18:34:24 -0500

    Update ClasePruebaBackImpl.java

--- modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
+++ modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
@@ -1,6 +1,6 @@
 package com.bbva.arq.front.spring.qpri.modulopruebaback.impl;
 
-import com.bbva.arq.front.spring.qpri.modulopruebaback.ClasePruebaBack;
+
 public class ClasePruebaBackImpl  implements  ClasePruebaBack {
 
 }
